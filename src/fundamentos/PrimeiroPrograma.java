c34187d481ffeabf1b67dd1d75b18c147b6f6d96
philippediniz <philippediniz@outlook.com>
2023-10-26 12:02:36 -0300

    segundo commit

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -4,6 +4,7 @@ public class PrimeiroPrograma {
 
     public static void main(String[] args) {
         System.out.println("Voltando a ativa com java ");
+        System.out.println("Voltando a ativa com java 2");
 
     }
 
