009743c1aca5209fab172490d8e7f6232523e9a2
philippediniz <philippediniz@outlook.com>
2023-10-31 09:18:14 -0300

    Organizando pastas e criando classes

--- src/fundamentos/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/fundamentos/PrimeiroPrograma.java
@@ -1,10 +1,11 @@
-package fundamentos;
+package fundamentos.fundamentos;
 
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
         System.out.println("Voltando a ativa com java ");
         System.out.println("Voltando a ativa com java 2");
+        System.out.println();
 
     }
 
