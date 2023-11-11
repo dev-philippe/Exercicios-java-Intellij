95d9d497c56f4a5fee6725d43cb57e87ed3dbcf2
unknown <philippediniz@outlook.com>
2023-11-11 12:36:19 -0300

    Inferência de tipos em java

---
+++ src/fundamentos/fundamentos/Inferencia.java
@@ -0,0 +1,33 @@
+package fundamentos.fundamentos;
+
+public class Inferencia {
+    public static void main(String[] args) {
+
+        double a = 4.5; // Variável declarada e inicialização;
+        System.out.println(a);
+
+        a = 12.57;
+        System.out.println(a);
+
+
+        var b = 4.5;
+        System.out.println(b);
+
+        var c = "Texto";
+        System.out.println(c);
+
+        c = "Outro texto";
+        System.out.println(c);
+
+        double d; // Variável foi declarada
+
+        d = 123.65; // variável inicializada;
+        System.out.println(d); // usada;
+
+        var f = 12; // inteiro;
+       // f = 12.01; Não aceita;
+
+
+
+    }
+}
