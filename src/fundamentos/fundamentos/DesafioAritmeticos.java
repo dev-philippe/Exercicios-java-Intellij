163c9e63d1cefd9c66805678944c278806b02478
philippediniz <philippediniz@outlook.com>
2023-12-11 11:22:14 -0300

    Exercício Java DesafioAritmeticos

---
+++ src/fundamentos/fundamentos/DesafioAritmeticos.java
@@ -0,0 +1,22 @@
+package fundamentos.fundamentos;
+
+public class DesafioAritmeticos {
+
+    public static void main(String[] args) {
+
+        int a =  3 * 4 - 10;
+        int b = (int)Math.pow(a,3 );
+        double c = (int)Math.pow(a,3 );
+
+        System.out.println(b);
+        System.out.println(c);
+
+
+
+
+
+
+
+
+    }
+}
