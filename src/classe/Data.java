28be1dbdc1d97543e0e1b2b4e8754a0ba92c4dc4
unknown <philippediniz@outlook.com>
2024-01-29 17:22:36 -0300

    Exercício Java de construtor Data

--- src/classe/Data.java
+++ src/classe/Data.java
@@ -6,11 +6,26 @@ public class Data {
     int mes;
     int ano;
 
-    String obterDataFormatada() {
-        return String.format("%d/%d/%d", dia ,mes, ano);
+
+    Data() {
+        dia = 1;
+        mes = 1;
+        ano = 1970;
+
     }
 
-    void imprimirDataFormatada(){
+    Data(int diaInicial, int mesInicial, int anoInicial) {
+        dia = diaInicial;
+        mes = mesInicial;
+        ano = anoInicial;
+
+    }
+
+    String obterDataFormatada() {
+        return String.format("%d/%d/%d", dia, mes, ano);
+    }
+
+    void imprimirDataFormatada() {
         System.out.println(obterDataFormatada());
     }
 
--- src/classe/DataTeste.java
+++ src/classe/DataTeste.java
@@ -4,16 +4,11 @@ public class DataTeste {
     public static void main(String[] args) {
 
         Data d1 = new Data();
-        d1.dia = 7;
-        d1.mes = 11;
-        d1.ano = 2021;
+        d1.ano = 2024;
 
-        System.out.printf("%d/%d/%d\n", d1.dia, d1.mes,d1.ano);
 
-        var d2 = new Data();
-        d2.dia = 31;
-        d2.mes = 12;
-        d2.ano = 2020;
+        var d2 = new Data(31, 12, 2020);
+
 
         String dataFortmata1 = d1.obterDataFormatada();
 
@@ -23,7 +18,7 @@ public class DataTeste {
 
 
         d1.imprimirDataFormatada();
-
+        d2.imprimirDataFormatada();
 
 
     }
